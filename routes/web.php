921d80429366238ff5d4ca41e2d1d04103e4ab87
Naufaliaaa <naufalzul47@gmail.com>
2024-12-06 23:06:26 +0700

    [PDF-8] FrontPage - Login

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontPage\HomeController;
 use App\Http\Controllers\FrontPage\AboutController;
 
+use App\Http\Controllers\FrontPage\LoginController;
+
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/login', [LoginController::class, 'showLogin']);
